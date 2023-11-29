b997320f3b4a7eaf23ca8738ff7d41ff4199c0d2
John Flanagan <github@jflan.com>
2023-11-28 18:46:16 -0600

    Defer content evaluation

--- Sources/Perception/WithPerceptionTracking.swift
+++ Sources/Perception/WithPerceptionTracking.swift
@@ -6,7 +6,6 @@ public enum PerceptionLocals {
 }
 
 @available(iOS, deprecated: 17, message: "TODO")
-@MainActor
 public struct WithPerceptionTracking<Content: View>: View {
   @State var id = 0
   let content: () -> Content
@@ -31,48 +30,49 @@ public struct WithPerceptionTracking<Content: View>: View {
   }
 }
 
+public struct DeferredContent<Content: View> {
+  let content: () -> Content
+}
+
 @resultBuilder
-@MainActor
 public enum ObservedBody {
   /// Wrap the final result in a `WithPerceptionTracking` view
-  public static func buildFinalResult<Content>(_ content: Content) -> WithPerceptionTracking<Content> {
-    WithPerceptionTracking<Content> { content }
+  public static func buildFinalResult<Content: View>(_ content: DeferredContent<Content>) -> WithPerceptionTracking<Content> {
+    WithPerceptionTracking<Content>(content: content.content)
   }
 
-  /// Pass through remaining result builder methods to `SwiftUI.ViewBuilder`
-
-  public static func buildExpression<Content: View>(_ content: Content) -> Content {
-    ViewBuilder.buildExpression(content)
+  public static func buildExpression<Content: View>(_ content: @autoclosure @escaping () -> Content) -> DeferredContent<Content> {
+    DeferredContent(content: content)
   }
 
-  public static func buildBlock() -> EmptyView {
-    ViewBuilder.buildBlock()
+  public static func buildBlock() -> DeferredContent<EmptyView> {
+    DeferredContent { EmptyView() }
   }
 
-  public static func buildBlock<Content: View>(_ content: Content) -> Content {
-    ViewBuilder.buildBlock(content)
+  public static func buildBlock<Content>(_ content: DeferredContent<Content>) -> DeferredContent<Content> {
+    content
   }
 
-  public static func buildBlock<each Content: View>(_ content: repeat each Content) -> TupleView<(repeat each Content)> {
-    ViewBuilder.buildBlock(repeat each content)
+  public static func buildBlock<each Content: View>(_ content: repeat DeferredContent<each Content>) -> DeferredContent<TupleView<(repeat each Content)>> {
+    DeferredContent(content: { ViewBuilder.buildBlock(repeat (each content).content()) })
   }
 
-  public static func buildIf<Content: View>(_ content: Content?) -> Content? {
-    ViewBuilder.buildIf(content)
+  public static func buildIf<Content: View>(_ content: DeferredContent<Content>?) -> DeferredContent<Content>? {
+    content
   }
 
-  public static func buildEither<TrueContent: View, FalseContent: View>(first: TrueContent) -> _ConditionalContent<TrueContent, FalseContent> {
-    ViewBuilder.buildEither(first: first)
+  public static func buildEither<TrueContent: View, FalseContent: View>(first: TrueContent) -> DeferredContent<_ConditionalContent<TrueContent, FalseContent>> {
+    DeferredContent(content: { ViewBuilder.buildEither(first: first) })
   }
 
-  public static func buildEither<TrueContent: View, FalseContent: View>(second: FalseContent) -> _ConditionalContent<TrueContent, FalseContent> {
-    ViewBuilder.buildEither(second: second)
+  public static func buildEither<TrueContent: View, FalseContent: View>(second: FalseContent) -> DeferredContent<_ConditionalContent<TrueContent, FalseContent>> {
+    DeferredContent(content: { ViewBuilder.buildEither(second: second) })
   }
 }
 
 @available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
 extension ObservedBody {
-  public static func buildLimitedAvailability<Content: View>(_ content: Content) -> AnyView {
-    ViewBuilder.buildLimitedAvailability(content)
+  public static func buildLimitedAvailability<Content: View>(_ content: DeferredContent<Content>) -> DeferredContent<AnyView> {
+    DeferredContent(content: { AnyView(content.content()) })
   }
 }
